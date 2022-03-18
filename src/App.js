af6019b76dbf1f1dc489755eadfe60bb61c37ef9
miguel <miguelcalvoguti@gmail.com>
2022-03-18 09:55:52 +0100

    fix import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import React, { Fragment, useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { v4 as uuid } from "uuid";
